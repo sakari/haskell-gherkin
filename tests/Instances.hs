e903b729af4d97704295cc7b5c6a7184acfbec81
Sakari Jokinen <sakariij@gmail.com>
2011-09-03 12:17:39 +0300

    proper names and tags are generated

--- tests/Instances.hs
+++ tests/Instances.hs
@@ -2,14 +2,36 @@
 
 module Instances where
 import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
 import Language.Gherkin
 import Control.Applicative
 
+tagChars :: String
+tagChars = ['a' .. 'z']
 
-tag :: Gen String
-tag = listOf1 $  elements $ ['a' .. 'z'] ++ 
-      ['0' .. '9']
+genTag :: Gen String
+genTag = listOf1 $ elements tagChars
 
+shrinkTags :: [String] -> [[String]]
+shrinkTags tags = shrinkList (shrinkList1 (`elem` tagChars)) tags
+    
+shrinkList1 :: Arbitrary a => (a -> Bool) -> [a] -> [[a]]
+shrinkList1 p ls = filter null $ fmap (filter p) $ shrink ls
+  
+shrinkName :: String -> [String]
+shrinkName name = shrinkList1 (`elem` nameChars) name
+
+nameChars :: String
+nameChars = ['a' .. 'z'] ++
+         ['0' .. '9'] ++
+         "+/"
+  
+genName :: Gen String
+genName = do
+  h <- elements nameChars  
+  t <- listOf $ elements (' ':nameChars)
+  e <- elements nameChars
+  return $ h:t ++ [e]
 
 instance Arbitrary Scenario where
   arbitrary = error "tbd"
@@ -18,8 +40,8 @@ instance Arbitrary Background where
   arbitrary = error "tbd"
   
 instance Arbitrary Feature where
-  arbitrary = Feature <$> listOf tag <*> 
-              return "feature" <*>
+  arbitrary = Feature <$> listOf genTag <*> 
+              genName <*>
               return "description" <*>
               return Nothing <*>
               return []
@@ -30,8 +52,8 @@ instance Arbitrary Feature where
     , feature_background
     , feature_scenarios
     } = tail' $ Feature <$> 
-        shrink feature_tags <*>
-        shrink feature_name <*>
+        shrinkTags feature_tags <*>
+        shrinkName feature_name <*>
         shrink feature_description <*>
         shrink feature_background <*>
         shrink feature_scenarios
