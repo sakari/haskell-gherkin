446fa3a80d250c52f5f6d5fca0e5c35f41025a6c
Sakari Jokinen <sakariij@gmail.com>
2012-05-21 13:26:21 +0300

    use cabal-dev + multicore for tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -7,11 +7,14 @@ import Text.Parsec.String
 import Instances ()
 import Language.Gherkin
 import Text.PrettyPrint
+import qualified GHC.Conc as Concurrent
 
 main :: IO ()
-main = defaultMain [testGroup "Parsing tests" tests
-                   , testGroup "Pretty roundtrip" prettyTests
-                   ]
+main = do
+  Concurrent.getNumProcessors >>= Concurrent.setNumCapabilities
+  defaultMain [testGroup "Parsing tests" tests
+              , testGroup "Pretty roundtrip" prettyTests
+              ]
 
 feature :: Feature
 feature = Feature { feature_tags = []
@@ -25,23 +28,23 @@ prop :: Parser a -> String -> a
 prop p str = case parse (p >>= \r -> eof >> return r) "" str of
   Left e -> error $ show e
   Right l -> l
-  
+
 (=.=) :: (Show a, Eq a) => a -> a -> Bool
 l =.= r | l /= r = error $ "Expected '"  ++ show r ++ "'\nGot '" ++ show l ++ "'"
         | otherwise = True
-                      
+
 
 prettyTests :: [Test]
 prettyTests = [
   testProperty "Feature" $ \f ->
    prop parseFeature (render $ pretty f) =.= f
-  
+
   , testProperty "Step" $ \s ->
    prop parseStep (render $ prettyStep s) =.= s
-  
+
   , testProperty "Scenario" $ \s ->
     prop (parseScenario <|> parseScenarioOutline) (render $ prettyScenario s) =.= s
-   
+
   , testProperty "BlockText" $ \b ->
    prop parseBlockText (render $ prettyBlock b) =.= b
   ]
@@ -50,11 +53,11 @@ tests :: [Test]
 tests = [
   testProperty "parse a row" $
   prop parseRow "|a|b|" =.= ["a", "b"]
-  
-  , testProperty "roundtrip for scenario with tables" $ 
+
+  , testProperty "roundtrip for scenario with tables" $
     let s = Scenario {scenario_name = ""
-                     , scenario_steps = 
-                       [Given (StepText "c" 
+                     , scenario_steps =
+                       [Given (StepText "c"
                              (Just (BlockTable (Table {table_headers = ["y"]
                                                       , table_values = [["p"]]
                                                       })
@@ -63,56 +66,56 @@ tests = [
                        ,Given (StepText "n" Nothing)
                        ]}
     in prop parseScenario (render $ prettyScenario s) =.= s
-       
+
   , testProperty "parse table" $
-  prop parseTable "| a | b |\n|c | d|" =.= 
-  Table { table_headers = [ "a", "b"  ]  
+  prop parseTable "| a | b |\n|c | d|" =.=
+  Table { table_headers = [ "a", "b"  ]
         , table_values =  [[ "c", "d" ]]
         }
-  
-  , testProperty "parse scenario-outline" $ 
+
+  , testProperty "parse scenario-outline" $
     prop parseFeature "Feature: feature\nScenario-outline: scenario\nGiven bar\nExamples:\n| header |\n|value|" =.=
-    feature { feature_scenarios = 
+    feature { feature_scenarios =
               [ScenarioOutline { scenario_name = "scenario"
-                               , scenario_steps = 
-                                 [Given $ StepText 
-                                  "bar" Nothing] 
-                               , scenario_table = 
-                                 Table { table_headers = 
+                               , scenario_steps =
+                                 [Given $ StepText
+                                  "bar" Nothing]
+                               , scenario_table =
+                                 Table { table_headers =
                                             ["header"]
                                        , table_values =
                                               [["value"]]
                                        }
                                }
               ]}
-    
+
   , testProperty "parse block argument to step" $
     prop parseStep "Given step:\n| a |\n| b|" =.=
     Given (StepText "step"
-           $ Just $ BlockTable $ 
+           $ Just $ BlockTable $
            Table { table_headers = ["a"]
                  , table_values = [["b"]]
                  }
           )
-    
+
   , testProperty "parse table argument" $
     prop parseBlockText "| a |\n|b|" =.=
-    (BlockTable $ Table { table_headers = ["a"] 
+    (BlockTable $ Table { table_headers = ["a"]
                         ,table_values = [["b"]]
                         })
-    
-  , testProperty "parse pystring argument" $ 
+
+  , testProperty "parse pystring argument" $
     prop parseBlockText "\"\"\"\nfoobar\nbar\n\"\"\"" =.=
     (BlockPystring "foobar\nbar")
-    
-  , testProperty "a single newline inside pystring" $ 
+
+  , testProperty "a single newline inside pystring" $
     prop parseBlockText "\"\"\"\n\n\n\"\"\"" =.=
     (BlockPystring "\n")
 
   , testProperty "pystrings are indented according to start quotes" $
-    prop parseBlockText (unlines $ 
-                         fmap ("  " ++) 
-                         ["\"\"\""   
+    prop parseBlockText (unlines $
+                         fmap ("  " ++)
+                         ["\"\"\""
                          , "No indent"
                          , " One indent"
                          , "\"\"\"" ]) =.=
@@ -121,7 +124,7 @@ tests = [
   , testProperty "parse feature tags" $
     prop parseFeature "@fst @snd\nFeature: feature\n" =.=
     feature { feature_tags = ["fst", "snd"] }
-    
+
   , testProperty "allow empty scenarios" $
     prop parseFeature "Feature: feature\nScenario: empty scenario\nScenario: second empty" =.=
     feature { feature_scenarios = [ Scenario { scenario_name = "empty scenario"
@@ -129,16 +132,16 @@ tests = [
                                   , Scenario { scenario_name = "second empty"
                                              , scenario_steps = []}
                                   ]}
-    
+
   , testProperty "parse feature description" $
     prop parseFeature "Feature: feature\ndescription first line\nsecond line" =.=
     feature { feature_description = "description first linesecond line" }
-  
-  , testProperty "parse feature with scenario" $ 
+
+  , testProperty "parse feature with scenario" $
     prop parseFeature  "\nFeature: feature\nScenario: a scenario\nGiven first step\nThen second step" =.= feature {
       feature_scenarios = [ Scenario { scenario_name = "a scenario"
-                                     , scenario_steps = 
-                                       [Given 
+                                     , scenario_steps =
+                                       [Given
                                         (StepText "first step" Nothing)
                                        , Then (StepText "second step" Nothing)
                                        ]
